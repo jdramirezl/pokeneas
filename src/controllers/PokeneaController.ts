a8a82a1802d703624c31e9c8ac3176a525c0b34a
jdramirezl <juliandavid.ramirez@mercadolibre.com.co>
2023-10-29 19:15:33 -0500

    Changed container name to container id

--- src/controllers/PokeneaController.ts
+++ src/controllers/PokeneaController.ts
@@ -2,30 +2,35 @@ import { Request, Response } from "express";
 import { PokeneaShow } from "../views/PokeneaShow";
 import { PokeneaService } from "../services/pokenea.service";
 import Pokenea from "../models/Pokenea";
+
 const os = require("os");
+
 export default class PokeneaController {
-    private pokeneaService: PokeneaService = new PokeneaService();
-    private containerName:string  = os.hostname();
-    public getAllPokeneas(req: Request, res: Response): void {
-        const pokeneas: Pokenea[] = this.pokeneaService.getPokeneas();
-        res.status(200).json({
-            pokeneas,
-            containerName: this.containerName,
-          });
-      }
-    public getPokenea(req:Request,res:Response):void{
-        const randomPokenea: Pokenea = this.pokeneaService.getRandomPokenea();
-        res.status(200).json({
-            pokenea: randomPokenea,
-            containerName: this.containerName,
-          });
-    }
-    public randomPokeneasInfoShow(req:Request,res:Response):void{
-        
-        const pokemon: Pokenea = this.pokeneaService.getRandomPokenea();
-        const containerName = this.containerName;
-        const  viewData= {pokemon,containerName};
-        const view= PokeneaShow(viewData);
-        res.status(200).send(view);
-    }
+  private pokeneaService: PokeneaService = new PokeneaService();
+  private containerId: string = os.hostname();
+
+  public getAllPokeneas(req: Request, res: Response): void {
+    const pokeneas: Pokenea[] = this.pokeneaService.getPokeneas();
+    res.status(200).json({
+      pokeneas,
+      containerId: this.containerId,
+    });
+  }
+
+  public getPokenea(req: Request, res: Response): void {
+    const randomPokenea: Pokenea = this.pokeneaService.getRandomPokenea();
+    res.status(200).json({
+      pokenea: randomPokenea,
+      containerId: this.containerId,
+    });
+  }
+
+  public randomPokeneasInfoShow(req: Request, res: Response): void {
+    const pokemon: Pokenea = this.pokeneaService.getRandomPokenea();
+    const containerName = this.containerId;
+    const viewData = { pokemon, containerName };
+    const view = PokeneaShow(viewData);
+    res.status(200).send(view);
+  }
 }
+
